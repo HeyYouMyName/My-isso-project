36b6ae90b829b60dcaa07f2854c6a8c0ce763005
Andrii Hrohul <grogulflash@gmail.com>
2024-02-10 03:05:21 +0400

    add main func

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from iss import ISS
 
 
-iss_tracker = ISS()
-print(iss_tracker.get_iss_country_currently_above())
-print(iss_tracker.create_map_iss_currently_above())
-print(iss_tracker.get_current_people_in_space())
+def main():
+    iss_tracker = ISS()
+    print(iss_tracker.get_iss_country_currently_above())
+    print(iss_tracker.create_map_iss_currently_above())
+    print(iss_tracker.get_current_people_in_space())
+
+
+if __name__ == "__main__":
+    main()
