7041e2870724d88d4b45b7fb7c68ff617d0b29aa
Andrii Hrohul <grogulflash@gmail.com>
2024-02-10 03:02:58 +0400

    create ISS class

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
-import time
-import requests
-from geopy.geocoders import Nominatim
-from datetime import datetime
+from iss import ISS
 
 
-def get_iss_current_position():
-    response = requests.get(url="http://api.open-notify.org/iss-now.json")
-    response.raise_for_status()
-    data = response.json()
-
-    iss_latitude = float(data["iss_position"]["latitude"])
-    iss_longitude = float(data["iss_position"]["longitude"])
-
-    return iss_latitude, iss_longitude
-
-
-# print(get_iss_current_position())
-
-
-def where_is_iss():
-    geolocator = Nominatim(user_agent="your_app_name")
-
-    latitude_longitude_list = get_iss_current_position()
-    latitude, longitude = get_iss_current_position()
-
-    address = geolocator.reverse((latitude, longitude))
-
-    country = address.address.get("country_code")
-
-    print(f"The country at these coordinates is: {country}")
-
-where_is_iss()
+iss_tracker = ISS()
+print(iss_tracker.get_iss_country_currently_above())
+print(iss_tracker.create_map_iss_currently_above())
+print(iss_tracker.get_current_people_in_space())
